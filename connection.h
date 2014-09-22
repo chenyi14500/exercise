d6ff15690243350c70b3d36c5b00004f9ddfb1a9
chenyi14500 <chenyi14500@gmail.com>
2014-09-22 05:17:17 +0000

    update connection.cpp file

--- connection.h
+++ connection.h
@@ -2,5 +2,17 @@ class connection
 {
 public:
 	int connect(char *ip, int port);
-	int accept(int listenfd);
+	int acceptConnection(int listenfd);
+  int buildServer(char *ip, int port);
 };
+int connection::connectServer(char *ip, int port) {
+  
+}
+
+int connection::acceptConnection(int listenfd) {
+  
+}
+
+int connection::buildServer(char *ip, int port) {
+  
+}
