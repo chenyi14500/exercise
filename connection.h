89f71aaa04a44248a91f40e1141da7c705421531
chenyi14500 <chenyi14500@gmail.com>
2014-09-22 05:44:28 +0000

    add new files

--- connection.h
+++ connection.h
@@ -1,18 +1,44 @@
 class connection
 {
 public:
-	int connect(char *ip, int port);
+	int connectConnection(char *ip, int port);
 	int acceptConnection(int listenfd);
   int buildServer(char *ip, int port);
+  void initAddress(char *ip, int port);
+public:
+   int sockfd;
+   struct sockaddr_in address;
+   socklen_t socklen;
 };
 int connection::connectServer(char *ip, int port) {
-  
+    initAddress(ip, port);
+    return connect(sockfd, (struct sockaddr *)&address, &socklen);
 }
 
-int connection::acceptConnection(int listenfd) {
-  
+int connection::acceptConnection() {
+   int conn = accpet(sockfd, &address, &socklen); 
+   return conn;
+}
+
+void connection::initAddress(char *ip, int port) {
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
 }
 
 int connection::buildServer(char *ip, int port) {
+    int ret = 0;
+    
+    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    
+    initAddress(ip, port);
   
+    ret = bind( sockfd, ( struct sockaddr * )&address, sizeof( address ) );
+    assert( ret >= 0 );
+
+    ret = listen( sockfd, 5 );
+    assert( ret >= 0 );
+  
+    return sockfd;
 }
--- server.cpp
+++ server.cpp
@@ -28,24 +28,10 @@ int main(int argc, char *argv[])
     const char* ip = argv[1];
     int port = atoi( argv[2] );
 
-    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
-    assert(listenfd >= 0);
-
-    struct linger tmp = {1, 0};
-    setsockopt(listenfd, SOL_SOCKET, SO_LINGER, &tmp, sizeof(tmp));
-
-    int ret = 0;
-    struct sockaddr_in address;
-    bzero(&address, sizeof(address));
-    address.sin_family = AF_INET;
-    inet_pton(AF_INET, ip, &address.sin_addr);
-    address.sin_port = htons(port);
-
-    ret = bind( listenfd, ( struct sockaddr * )&address, sizeof( address ) );
-    assert( ret >= 0 );
-
-    ret = listen( listenfd, 5 );
-    assert( ret >= 0 );
+    int listenfd = connection::buildServer(ip, port);
+    epoll server_epoll(MAX_EVENT_NUMBER, 3000);
+    server_epoll.add(listenfd);
+  
 
     struct epoll_event events[MAX_EVENT_NUMBER];
     int epollfd = epoll_create(5); 
